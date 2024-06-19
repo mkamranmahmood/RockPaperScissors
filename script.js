48a01b1cbde73a10477c8f681901ad858a8bb272
mkamranmahmood <62491042+mkamranmahmood@users.noreply.github.com>
2024-06-19 18:20:46 +0100

    Added a UI to the Rock Paper Scissors game

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let arr = ["Rock", "Paper", "Scissors"]
 let humanScore = 0;
 let computerScore = 0;
 
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.id);
+    });
+});
+
+
 function getComputerChoice() {
     return arr[Math.round(Math.random()*2)];
 }
@@ -16,50 +25,83 @@ function titleCase(string) {
     return string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
 
-function playRound() {
+function playRound(string) {
     let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-    
-    console.log("The computer chose: " + computerChoice)
-    console.log("The human chose: " + humanChoice);
+    let humanChoice = string[0].toUpperCase() + string.slice(1).toLowerCase();
+    let result="";
+
+    //console.log("The computer chose: " + computerChoice)
+    document.getElementById("humanchoice").innerHTML = humanChoice;
+    //console.log("The human chose: " + humanChoice);
+    document.getElementById("computerchoice").innerHTML = computerChoice;
 
     if (humanChoice == "Rock" && computerChoice == "Rock") {
-        console.log("Tie")
+        //console.log("Tie")
+        result="Tie"
+        document.getElementById("result").innerHTML = result;
     } else if (humanChoice == "Rock" && computerChoice == "Paper") {
-        console.log("Computer wins")
+        //console.log("Computer wins")
+        result="Computer wins"
+        document.getElementById("result").innerHTML = result;
         computerScore++;
     } else if (humanChoice == "Rock" && computerChoice == "Scissors") {
-        console.log("Human wins")
+        //console.log("Human wins")
+        result="Human wins"
+        document.getElementById("result").innerHTML = result;
         humanScore++;
     } else if (humanChoice == "Paper" && computerChoice == "Rock") {
-        console.log("Human wins")
+        //console.log("Human wins")
+        result="Human wins"
+        document.getElementById("result").innerHTML = result;
         humanScore++;
     } else if (humanChoice == "Paper" && computerChoice == "Paper") {
-        console.log("Tie")
+        //console.log("Tie")
+        result="Tie"
+        document.getElementById("result").innerHTML = result;
     } else if (humanChoice == "Paper" && computerChoice == "Scissors") {
-        console.log("Computer wins")
+        //console.log("Computer wins")
+        result="Computer wins"
+        document.getElementById("result").innerHTML = result;
         computerScore++;
     } else if (humanChoice == "Scissors" && computerChoice == "Rock") {
-        console.log("Computer wins")
+        //console.log("Computer wins")
+        result="Computer wins"
+        document.getElementById("result").innerHTML = result;
         computerScore++;
     } else if (humanChoice == "Scissors" && computerChoice == "Paper") {
-        console.log("Human wins")
+        //console.log("Human wins")
+        result="Human wins"
+        document.getElementById("result").innerHTML = result;
         humanScore++;
     } else if (humanChoice == "Scissors" && computerChoice == "Scissors") {
-        console.log("Tie")
+        //console.log("Tie")
+        result="Tie"
+        document.getElementById("result").innerHTML = result;
     } else {
-        console.log("Your entry was invalid ")
+        //console.log("Your entry was invalid ")
+        result="Your entry was invalid"
+        document.getElementById("result").innerHTML = result;
     }
 
-    console.log("Human Score: " + humanScore);
-    console.log("Computer Score: " + computerScore);
+    //console.log("Human Score: " + humanScore);
+    document.getElementById("humanscore").innerHTML = humanScore;
+    //console.log("Computer Score: " + computerScore);
+    document.getElementById("computerscore").innerHTML = computerScore;
+
+    if (humanScore >= 5) {
+        alert("Game over! You have won!")
+    } else if (computerScore >= 5) {
+        alert("Game over! The computer has won!")
+    }
+
 }
 
 function playGame() {
-    for (let i = 1; i <= 5; i++) {
-        console.log("Game: " + i);
+    /*for (let i = 1; i <= 5; i++) {
+        console.log("Game: " + i);*/
         playRound()
-    }
+    /*}*/
 }
 
-playGame();
+//playGame();
+
